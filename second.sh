8e56e78892d129f8449009166cd1330a52105d4e
somogibbs <somogibbs@gmail.com>
2025-04-18 08:13:55 -0500

    Create second.sh

---
+++ second.sh
@@ -0,0 +1,4 @@
+# Install extra gnome components
+sudo apt -y install gnome-shell-extension-manager gnome-shell-extension-dashtodock gnome-tweaks libproxy1-plugin-networkmanager network-manager-gnome file-roller totem-plugins gnome-photos
+# Other apt installs
+sudo apt -y install libavcodec-extra ffmpeg mpv cpu-x lm-sensors neofetch nala
