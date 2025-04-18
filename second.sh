225adaa590d55424a93f1cf42a16e572d886163d
somogibbs <somogibbs@gmail.com>
2025-04-18 10:03:37 -0500

    Delete second.sh

--- second.sh
+++
@@ -1,5 +0,0 @@
-# Install extra gnome components
-sudo apt -y install gnome-shell-extension-manager gnome-shell-extension-dashtodock gnome-tweaks gnome-extra-icons libproxy1-plugin-networkmanager 
-network-manager-gnome file-roller totem-plugins gnome-photos
-# Other apt installs
-sudo apt -y install libavcodec-extra ffmpeg mpv cpu-x lm-sensors neofetch nala
