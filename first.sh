c25fe779a951bc8ecdc0a61db6b4d86958899d74
somogibbs <somogibbs@gmail.com>
2025-04-18 09:50:49 -0500

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,3 +1,9 @@
 !#/bin/bash
-# Install Gnome core - remove initial installer - reboot
-sudo apt update && sudo apt install gnome-core -y && sudo apt purge ifupdown -y && sudo shutdown -r now
+# Install Gnome components 
+sudo apt update && sudo apt install gnome-core -y && sudo apt install gnome-shell-extension-manager 
+gnome-shell-extension-dashtodock gnome-tweaks libproxy1-plugin-networkmanager network-manager-gnome 
+file-roller totem-plugins gnome-photos -y 
+# Miscellaneous program install
+sudo apt -y install libavcodec-extra ffmpeg mpv
+# Remove initial installer - reboot
+sudo apt purge ifupdown -y && sudo shutdown -r now
