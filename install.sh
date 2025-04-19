597ac2f19bb297b24117c0209e08a68f95f6349d
somogibbs <somogibbs@gmail.com>
2025-04-19 11:27:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 !#/bin/bash
 ## Install Gnome components 
-sudo apt update && sudo apt install gnome-core -y && sudo apt install gnome-shell-extension-manager 
-gnome-shell-extension-dashtodock gnome-tweaks libproxy1-plugin-networkmanager network-manager-gnome 
-file-roller totem-plugins gnome-photos gnome-extra-icons -y 
+sudo apt update && sudo apt install gnome-core -y && sudo apt install gnome-tweaks gnome-shell-extension-manager 
+gnome-shell-extension-dashtodock libproxy1-plugin-networkmanager network-manager-gnome 
+file-roller gnome-photos gnome-extra-icons -y 
 
 ## Miscellaneous program install
 sudo apt -y install libavcodec-extra ffmpeg mpv cpu-x lm-sensors neofetch nala xfce4-terminal btop firefox-esr 
@@ -10,7 +10,7 @@ sudo apt -y install libavcodec-extra ffmpeg mpv cpu-x lm-sensors neofetch nala x
 #Install Joplin
 wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
 
-#Install Bitwarden
+#Download Bitwarden package
 wget "https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb" -O bitwarden.deb
 
 #Install Speedtest
