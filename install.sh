29cd38fd09794200b3d02f963769e13377c3c824
somogibbs <somogibbs@gmail.com>
2025-04-26 18:18:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,21 @@
 sudo apt update && sudo apt upgrade -y 
 
 ## Install Gnome components 
-sudo apt install gnome-core -y && sudo apt install gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-dashtodock gnome-extra-icons libproxy1-plugin-networkmanager network-manager-gnome 
-file-roller -y 
+sudo apt install gnome-core -y && sudo apt install gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-dashtodock gnome-extra-icons libproxy1-plugin-networkmanager network-manager-gnome file-roller -y 
 
 ## Miscellaneous program install
 sudo apt -y install libavcodec-extra ffmpeg mpv cpu-x lm-sensors neofetch nala xfce4-terminal btop firefox-esr obsidian-icon-theme fonts-recommended
 
-#Install Speedtest
+# Install Speedtest
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash &&
 sudo apt -y install speedtest
 
-#Setup UFW
+# Setup UFW
 sudo apt -y install ufw 
-sudo ufw enable
+sudo ufw enable 
+
+# Download Bitwarden debian package
+wget "https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb" -O bitwarden.deb
 
 ## Configure wifi - reboot
 sudo apt purge ifupdown -y && 
