63f94b46250ab70979379e0e07b35c477b169ad3
somogibbs <somogibbs@gmail.com>
2025-04-26 18:19:26 -0500

    Update after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -1,6 +1,5 @@
 !#/bin/bash
 # Finishing touches after main install of system. 
 sudo apt remove gnome-contacts gnome-terminal totem &&
-sudo apt install gnome-shell-extension-dashtodock gnome-extra-icons obsidian-icon-theme fonts-recommended -y 
 sudo shutdown -r now
 
