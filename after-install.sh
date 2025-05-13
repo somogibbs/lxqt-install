d08dafb18467c2128852a256fcba59a74487f6c4
somogibbs <somogibbs@gmail.com>
2025-05-13 02:31:11 -0500

    Update after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -2,6 +2,7 @@
 # Finishing touches after main install of system. 
 sudo apt remove gnome-contacts gnome-terminal totem &&
 sudo apt auto remove 
+
 # Create and move folders
 mkdir ~/.themes
 mv ~/mn-gnome/themes.zip ~/.themes
