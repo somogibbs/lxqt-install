e6a2c35693eb797395d92e097d7dcaba1cb85e8a
somogibbs <somogibbs@gmail.com>
2025-06-08 11:14:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install gnome-core -y && sudo apt install gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-dashtodock gnome-shell-extension-tiling-assistant gnome-shell-extension-blur-my-shell gnome-extra-icons libproxy1-plugin-networkmanager network-manager-gnome file-roller -y 
 
 ## Miscellaneous program install
-sudo apt -y install libavcodec-extra ffmpeg mpv cpu-x lm-sensors neofetch nala xfce4-terminal btop firefox-esr obsidian-icon-theme fonts-recommended variety 
+sudo apt -y install libavcodec-extra ffmpeg mpv cpu-x lm-sensors neofetch xfce4-terminal btop firefox-esr obsidian-icon-theme fonts-recommended variety 
 
 # Install Speedtest
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash &&
@@ -18,6 +18,9 @@ sudo ufw enable
 
 # Download Bitwarden debian package
 wget "https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb" -O bitwarden.deb
+
+# Install Joplin
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
 
 ## Configure wifi - reboot
 sudo apt purge ifupdown -y && 
